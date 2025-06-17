95571e7b714934cfac6ce6b6aed976a31cfffd12
Carlos Giammaria <carlosgiammaria29@gmail.com>
2025-06-17 15:39:50 -0500

    agregando netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify"; // 👈 Agregado
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [react()],
+  integrations: [
+    react(),
+    netlify(), // 👈 Integración de Netlify
+  ],
 });
