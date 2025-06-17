574c7e95ad2ddcafa9199440b856d98ee2f6294c
Carlos Giammaria <carlosgiammaria29@gmail.com>
2025-06-17 15:21:06 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const informationServices = [
+  {
+    titulo: "Gestión Administrativa",
+    texto:
+      "Incluye manejo de reuniones, documentación legal, custodia de documentos, registro de propietarios y gestión del presupuesto. También se coordina la ejecución de contratos para mantenimiento, aseo y vigilancia conforme al presupuesto aprobado por el consejo. Se asegura el cumplimiento de normas internas por parte de los residentes.",
+    img: "/src/img/gestionadmin.png",
+    reverse: false,
+  },
+  {
+    titulo: "Gestión Operativa",
+    texto:
+      "Supervisión del estado físico de la copropiedad, mantenimiento preventivo y correctivo, programación y ejecución de servicios especializados como plomería, jardinería, limpieza de fachadas y más. Se garantiza la conservación y funcionamiento de motobombas, ascensores, red contra incendios y zonas comunes.",
+    img: "/src/img/gestionoper.jpg",
+    reverse: true,
+  },
+  {
+    titulo: "Servicios Contables",
+    texto:
+      "Manejo contable profesional realizado por un Contador Público con experiencia en propiedad horizontal. Incluye facturación de cuotas, elaboración de estados financieros, conciliaciones bancarias y presentación de informes al consejo y revisor fiscal. Toda la información es precisa y oportuna.",
+    img: "/src/img/servicios_contables.jpg",
+    reverse: false,
+  },
+  {
+    titulo: "Recaudo de Cartera",
+    texto:
+      "Gestión diaria del recaudo, aplicación de pagos, actualización de cartera y seguimiento de morosos. Se generan reportes con indicadores de recuperación. El proceso incluye notificación, conciliación y, si es necesario, gestión jurídica para cobro por la vía legal.",
+    img: "/src/img/recaudocarte.jpg",
+    reverse: true,
+  },
+  {
+    titulo: "Asesoría Jurídica",
+    texto:
+      "Nuestro departamento jurídico especializado asesora en normativas de propiedad horizontal, sanciones, elaboración de manuales de convivencia, reformas al reglamento, derechos de petición, y procesos por cartera vencida. La abogada asignada representa legalmente a la copropiedad cuando es necesario.",
+    img: "/src/img/asesoriajuri.jpg",
+    reverse: false,
+  },
+  {
+    titulo: "Asambleas y Consejo",
+    texto:
+      "Convocamos y organizamos asambleas ordinarias y extraordinarias, elaboramos actas, y presentamos informes de gestión detallados. Además, nos reunimos mensualmente con el Consejo de Administración para revisar avances y resolver inquietudes, documentando cada sesión con profesionalismo.",
+    img: "/src/img/asamblea.jpg",
+    reverse: true,
+  },
+]
